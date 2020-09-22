a1eb1e30a58b5bc1db15405aeed0db5890c4b451
Shubham <babukumar9931@gmail.com>
2020-09-22 20:19:49 +0530

    Added GeoLocation functionality

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:io';
+import 'package:geolocator/geolocator.dart';
 
 class WeatherApp extends StatefulWidget {
   @override
@@ -8,40 +10,93 @@ class WeatherApp extends StatefulWidget {
 }
 
 class _WeatherAppState extends State<WeatherApp> {
-  int temperature = 0;
+  int temperature;
   String location = 'San Francisco';
   int woeid = 2487956;
   String weather = 'clear';
+  String abbreviation = '';
+  String errorMessage = '';
+
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+  Position _currentPosition;
+  String _currentAddress;
 
   String searchApiUrl =
       'https://www.metaweather.com/api/location/search/?query=';
   String locationApiUrl = 'https://www.metaweather.com/api/location/';
 
-  void fetchSearch(String input) async {
-    var searchResult = await http.get(searchApiUrl + input);
-    var result = json.decode(searchResult.body)[0];
+  initState() {
+    super.initState();
+    fetchLocation();
+  }
 
-    setState(() {
-      location = result["title"];
-      woeid = result["woeid"];
-    });
+  void fetchSearch(String input) async {
+    try {
+      var searchResult = await http.get(searchApiUrl + input);
+      var result = json.decode(searchResult.body)[0];
+
+      setState(() {
+        location = result["title"];
+        woeid = result["woeid"];
+        errorMessage = '';
+      });
+    } catch (error) {
+      setState(() {
+        errorMessage =
+            "Sorry, we don't have data about this city. Try another one.";
+      });
+    }
   }
 
   void fetchLocation() async {
     var locationResult = await http.get(locationApiUrl + woeid.toString());
     var result = json.decode(locationResult.body);
-    var consolidated_weather = result["consolidated_weather"];
-    var data = consolidated_weather[0];
+    var consolidatedWeather = result["consolidated_weather"];
+    var data = consolidatedWeather[0];
 
     setState(() {
       temperature = data["the_temp"].round();
       weather = data["weather_state_name"].replaceAll(' ', '').toLowerCase();
+      abbreviation = data["weather_state_abbr"];
     });
   }
 
-  void onTextFieldSubmitted(String input) {
+  void onTextFieldSubmitted(String input) async {
     fetchSearch(input);
     fetchLocation();
+  }
+
+  _getCurrentLocation() {
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+
+      _getAddressFromLatLng();
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> p = await geolocator.placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+
+      Placemark place = p[0];
+
+      setState(() {
+        _currentAddress =
+            "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
+      onTextFieldSubmitted(place.locality);
+      print(place.locality);
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
@@ -55,50 +110,90 @@ class _WeatherAppState extends State<WeatherApp> {
               fit: BoxFit.cover,
             ),
           ),
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Center(
-                      child: Text(
-                        temperature.toString() + ' °C',
-                        style: TextStyle(color: Colors.white, fontSize: 60.0),
-                      ),
-                    ),
-                    Center(
-                      child: Text(
-                        location,
-                        style: TextStyle(color: Colors.white, fontSize: 40.0),
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Container(
-                      width: 300,
-                      child: TextField(
-                        onSubmitted: (String input) {
-                          onTextFieldSubmitted(input);
-                        },
-                        style: TextStyle(color: Colors.white, fontSize: 25),
-                        decoration: InputDecoration(
-                          hintText: 'Search another location...',
-                          hintStyle:
-                              TextStyle(color: Colors.white, fontSize: 18.0),
-                          prefixIcon: Icon(Icons.search, color: Colors.white),
+          child: temperature == null
+              ? Center(child: CircularProgressIndicator())
+              : Scaffold(
+                  appBar: AppBar(
+                    actions: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            _getCurrentLocation();
+                          },
+                          child: Icon(Icons.location_city, size: 36.0),
                         ),
+                      )
+                    ],
+                    backgroundColor: Colors.transparent,
+                    elevation: 0.0,
+                  ),
+                  resizeToAvoidBottomInset: false,
+                  backgroundColor: Colors.transparent,
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Center(
+                            child: Image.network(
+                              'https://www.metaweather.com/static/img/weather/png/' +
+                                  abbreviation +
+                                  '.png',
+                              width: 100,
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              temperature.toString() + ' °C',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 60.0),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              location,
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 40.0),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          )),
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            width: 300,
+                            child: TextField(
+                              onSubmitted: (String input) {
+                                onTextFieldSubmitted(input);
+                              },
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 25),
+                              decoration: InputDecoration(
+                                hintText: 'Search another location...',
+                                hintStyle: TextStyle(
+                                    color: Colors.white, fontSize: 18.0),
+                                prefixIcon:
+                                    Icon(Icons.search, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(right: 32.0, left: 32.0),
+                            child: Text(errorMessage,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.redAccent,
+                                    fontSize:
+                                        Platform.isAndroid ? 15.0 : 20.0)),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                )),
     );
   }
 }
